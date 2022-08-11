b46afa8cc170e074f0eacc909fd6311205e1aea4
hopscott <scott.willis2706@gmail.com>
2022-08-11 17:05:52 +0200

    #2

--- include/minirt.h
+++ include/minirt.h
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 18:31:50 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/11 15:49:30 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/11 17:05:17 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,129 @@
 # include <sys/stat.h>
 # include <fcntl.h>
 # include <stdio.h>
+
+/* type of objects in linked list */
+
+enum {
+	ERROR		= -1,
+	AMBIENT		= 0,
+	CAMERA		= 1,
+	LIGHT		= 2,
+	SPHERE		= 3,
+	PLANE		= 4,
+	CYLINDER	= 5
+};
+
+/* Use void pointers in linked list to build up objects */
+
+typedef struct s_list
+{
+	int				type;
+	void			*content;
+	struct s_list	*next;
+}			t_list;
+
+/* identifier: A								 	*/
+/* ambient lighting ratio in range [0.0,1.0]: 0.2	*/
+/* R,G,B colors in range [0-255]: 255, 255, 255 	*/
+
+typedef struct s_ambient
+{
+	double	lighting_ratio;
+	t_size	r;
+	t_size	g;
+	t_size	b;
+}			t_ambient;
+
+/* identifier: C 												*/
+/* x,y,z coordinates of the view point: 0.0,0.0,20.6 			*/
+/* 3d normalized orientation vector.					 		*/
+/* In range [-1,1] for each x,y,z axis: 0.0,0.0,1.0 			*/
+/* FOV : Horizontal field of view in degrees in range [0,180] 	*/
+
+typedef struct s_camera
+{
+	double	x;
+	double	y;
+	double	z;
+	double	vec_x;
+	double	vec_y;
+	double	vec_z;
+	t_size	fov;
+}			t_camera;
+
+/* identifier: L 												*/
+/* x,y,z coordinates of the light point: 0.0,0.0,20.6			*/
+/* the light brightness ratio in range [0.0,1.0]: 0.6	 		*/
+/* (not mandatory)R,G,B colors in range [0-255]: 10, 0, 255 	*/
+
+typedef struct s_light
+{
+	double	x;
+	double	y;
+	double	z;
+	double	brightness_ratio;
+	t_size	r;
+	t_size	g;
+	t_size	b;
+}			t_light;
+
+/* identifier: sp											*/
+/* x,y,z coordinates of the sphere center: 0.0,0.0,20.6		*/
+/* the sphere diameter: 12.6								*/
+/* R,G,B colors in range [0-255]: 10, 0, 255			 	*/
+
+typedef struct s_sphere
+{
+	double	x;
+	double	y;
+	double	z;
+	double	diameter;
+	t_size	r;
+	t_size	g;
+	t_size	b;
+}			t_sphere;
+
+/* identifier: pl										*/
+/* x,y,z coordinates: 0.0,0.0,-10.0						*/
+/* 3d normalized orientation vector. 					*/
+/* In range [-1,1] for each x,y,z axis: 0.0,0.0,1.0		*/
+/* R,G,B colors in range [0-255]: 0, 0, 255		 		*/
+
+typedef struct s_plane
+{
+	double	x;
+	double	y;
+	double	z;
+	double	vec_x;
+	double	vec_y;
+	double	vec_z;
+	t_size	r;
+	t_size	g;
+	t_size	b;
+}			t_plane;
+
+/* identifier: cy										*/
+/* x,y,z coordinates: 50.0,0.0,20.6						*/
+/* 3d normalized orientation vector.  					*/
+/* In range [-1,1] for each x,y,z axis: 0.0,0.0,1.0		*/
+/* the cylinder diameter: 14.2					 		*/
+/* the cylinder height: 21.42					 		*/
+/* R,G,B colors in range [0,255]: 10, 0, 255	 		*/
+
+typedef struct s_cylinder
+{
+	double	x;
+	double	y;
+	double	z;
+	double	vec_x;
+	double	vec_y;
+	double	vec_z;
+	double	diameter;
+	double	height;
+	t_size	r;
+	t_size	g;
+	t_size	b;
+}			t_cylinder;
 
 #endif
--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,38 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 13:46:19 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/11 14:27:26 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/11 16:25:51 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+	char	*path;
+	int		fd;
+	char	*str;
+	char	**tbl;
+
+	if (ac != 2)
+		return (1);
+	path = av[1];
+	fd = open(path, O_RDONLY);
+	str = get_next_line(fd);
+	while (str)
+	{
+		tbl = ft_split(str, ' ');
+		if ((ft_strncmp(tbl[0], "A", 2)) == 0)
+			printf("Ambient lighting\n");
+		else if ((ft_strncmp(tbl[0], "C", 2)) == 0)
+			printf("Camera\n");
+		else if ((ft_strncmp(tbl[0], "L", 2)) == 0)
+			printf("Light\n");
+		else if ((ft_strncmp(tbl[0], "sp", 3)) == 0)
+			printf("Sphere\n");
+		else if ((ft_strncmp(tbl[0], "pl", 3)) == 0)
+			printf("Plane\n");
+		else if ((ft_strncmp(tbl[0], "cy", 3)) == 0)
+			printf("Cylinder\n");
+		ft_freetbl(tbl, -1);
+		free(str);
+		str = get_next_line(fd);
+	}
+	close(fd);

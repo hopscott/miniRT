9b8c6f97279d1c71f065600cdf2773decb4ebb53
hopscott <scott.willis2706@gmail.com>
2022-08-12 16:07:41 +0200

    #5 cleanup

--- src/parsing/parser.c
+++ src/parsing/parser.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/11 13:46:19 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/11 23:14:14 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/12 16:07:04 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,19 +59,25 @@ void	ft_print_objects(t_list **lst)
 				obj->a.lighting_ratio, obj->a.r, obj->a.g, obj->a.b);
 		else if (elem->type == CAMERA)
 			printf("CAMERA\t\t=> %f,%f,%f %f,%f,%f %zu \n", \
-			obj->c.x, obj->c.y, obj->c.z, obj->c.vec_x, obj->c.vec_y, obj->c.vec_z, obj->c.fov);
+			obj->c.x, obj->c.y, obj->c.z, obj->c.vec_x, obj->c.vec_y, \
+			obj->c.vec_z, obj->c.fov);
 		else if (elem->type == LIGHT)
 			printf("LIGHT\t\t=> %f,%f,%f %f %zu,%zu,%zu \n", \
-			obj->l.x, obj->l.y, obj->l.z, obj->l.brightness_ratio, obj->l.r, obj->l.g, obj->l.b);
+			obj->l.x, obj->l.y, obj->l.z, obj->l.brightness_ratio, obj->l.r, \
+			obj->l.g, obj->l.b);
 		else if (elem->type == SPHERE)
 			printf("SPHERE\t\t=> %f,%f,%f %f %zu,%zu,%zu \n", \
-			obj->sp.x, obj->sp.y, obj->sp.z, obj->sp.diameter, obj->sp.r, obj->sp.g, obj->sp.b);
+			obj->sp.x, obj->sp.y, obj->sp.z, obj->sp.diameter, obj->sp.r, \
+			obj->sp.g, obj->sp.b);
 		else if (elem->type == PLANE)
 			printf("PLANE\t\t=> %f,%f,%f %f,%f,%f %zu,%zu,%zu \n", \
-			obj->pl.x, obj->pl.y, obj->pl.z, obj->pl.vec_x, obj->pl.vec_y, obj->pl.vec_z, obj->pl.r, obj->pl.g, obj->pl.b);
+			obj->pl.x, obj->pl.y, obj->pl.z, obj->pl.vec_x, obj->pl.vec_y, \
+			obj->pl.vec_z, obj->pl.r, obj->pl.g, obj->pl.b);
 		else if (elem->type == CYLINDER)
 			printf("CYLINDER\t=> %f,%f,%f %f,%f,%f %f %f %zu,%zu,%zu \n", \
-			obj->cy.x, obj->cy.y, obj->cy.z, obj->cy.vec_x, obj->cy.vec_y, obj->cy.vec_z, obj->cy.diameter, obj->cy.height, obj->cy.r, obj->cy.g, obj->cy.b);
+			obj->cy.x, obj->cy.y, obj->cy.z, obj->cy.vec_x, obj->cy.vec_y, \
+			obj->cy.vec_z, obj->cy.diameter, obj->cy.height, obj->cy.r, \
+			obj->cy.g, obj->cy.b);
 		elem = elem->next;
 	}
 }
--- src/vec3/adv_ops.c
+++ src/vec3/adv_ops.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 12:56:59 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/12 16:03:49 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/12 16:07:23 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,8 +54,25 @@ t_vec3	*vec3_unit(t_vec3 *vec1)
 
 void	vec3_print(t_vec3 *vec)
 {
-	printf("/ %f \\\n", vec->e[0]); 
+	printf("/ %f \\\n", vec->e[0]);
 	printf("| %f |\n", vec->e[1]);
 	printf("\\ %f /\n", vec->e[2]);
 }
 
+/*
+	t_vec3 *v1 = vec3_init(1, 2, 3);
+	t_vec3 *v2 = vec3_init(30, 20, 10);
+	
+	printf("CROSS\n");
+	vec3_print(vec3_cross(v1, v2));
+	printf("ADD\n");
+	vec3_print(vec3_add(v1, v2));
+	printf("SUBTRACT\n");
+	vec3_print(vec3_subtract(v1, v2));
+	printf("DIVIDE\n");
+	vec3_print(vec3_divide(v1, 3));
+	printf("MULTIPLY\n");
+	vec3_print(vec3_multiply(v1, 3));
+	printf("DOT\n");
+	printf("%f\n", vec3_dot(v1, v2));
+*/

c39553d09e70415b6a57daf937fe51ffdeabfb6b
hopscott <scott.willis2706@gmail.com>
2022-08-17 23:48:18 +0200

    #6 working on basic tracing of sphere

--- src/camera/rays.c
+++ src/camera/rays.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 20:17:24 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/17 16:29:23 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/17 23:47:07 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,64 +52,63 @@
 //     return hitColor; 
 // } 
 
-t_list	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_list **objects)
+t_obj_list	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_obj_list **objects)
 {
-	double		t;
 	double		tmin;
-	t_list		*elem;
+	t_obj_list	*nearest;
+	t_obj_list	*elem;
 	t_object	*obj;
+	t_vec3		*vec;
 
-	// iterate over each object to see if vec of len t intersects
-	t = INFINITY;
-	tmin = t;
-	elem = *lst;
+	tmin = INFINITY;
+	*nearest = NULL;
+	elem = *objects;
 	while (elem)
 	{
 		obj = (t_object *)(elem->content);
-		if (((elem->type == LIGHT) && light_intersects(origin, direction, t)) && (t < tmin))
-			tmin = t;
-		else if (elem->type == SPHERE)
-			continue ;
+		if (elem->type == SPHERE)
+		{
+			vec = vec3_init(obj->sp->x, obj->sp->y, obj->sp->z);
+			tmin = vec3_distance_points(origin, vec);
+			free(vec);
+			nearest = elem;
+		}
 		else if (elem->type == PLANE)
 			continue ;
 		else if (elem->type == CYLINDER)
 			continue ;
 		elem = elem->next;
 	}
-	return (elem);
+	return (nearest);
 }
 
-t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_list **objects)
+t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_space *space)
 {
-	t_vec3		*rgb;
-	t_list		*elem;
-	t_object	*obj;
+	t_obj_list		*olst;
+	t_object		*obj;
+	t_vec3			*rgb;
+	// t_light			*light;
 
-	elem = nearest_hit_object(origin, direction, objects);
-	if (elem)
+	// light = nearest_hit_light(origin, direction, space->lights);
+	olst = nearest_hit_object(origin, direction, space->objects);
+	if (olst)
 	{		
 		obj = (t_object *)(elem->content);
-		
-		// hits light directly
-		if (elem->type == LIGHT)
-			rgb = vec3_init(obj->l.r * brightness_ratio, obj->l.g * brightness_ratio, obj->l.b * brightness_ratio);
-		
-		// hits other objects so shadow
-		else if (elem->type == SPHERE)
-			continue ;
+		if (elem->type == SPHERE)
+			rgb = vec3_init(obj->sp->r, obj->sp->g, obj->sp->b);
 		else if (elem->type == PLANE)
 			continue ;
 		else if (elem->type == CYLINDER)
 			continue ;
 	}
-	
-	// if no object hit = ambient lighting (-> need to set in a global data struct not linked list)
 	else
-		rgb = vec3_init(obj->a.r * lighting_ratio, obj->a.g * lighting_ratio, obj->a.b * lighting_ratio);
+		rgb = vec3_init(space->ambient->r * space->ambient->lighting_ratio, \
+						space->ambient->g * space->ambient->lighting_ratio, \
+						space->ambient->b * space->ambient->lighting_ratio);
 	return (rgb);
 }
 
-void	render(int width, int height, t_camera *camera, t_list **objects)
+void	render(int width, int height, t_space *space)
 {
 	int		px;
 	int		py;
@@ -120,22 +119,18 @@ void	render(int width, int height, t_camera *camera, t_list **objects)
 	t_vec3	*direction;
 	t_vec3	*rgb;
 
-	// convert camera origin point from ccs to wcs
-	mat = camera_lookat(camera);
+	mat = camera_lookat(space->camera);
 	origin = vec3_matrix_multiply(mat, vec3_init(0, 0, 0), 0);
-	// convert each point on camera canvas in ccs to wcs
 	py = -1;
 	while (++py < height)
 	{
 		px = -1;
 		while (++px < width)
 		{
-			// + 0.5 to aim at middle of pixel -> -1 to prevent overflow of mlx image
 			x = ((px + 0.5) / width) - 1;
 			y = ((py + 0.5) / height) - 1;
 			direction = vec3_matrix_multiply(mat, vec3_init(x, y, -1), 1);
-			rgb = cast_ray(origin, direction, objects);
-
+			rgb = cast_ray(origin, direction, space);
 			// colour_pixel(img->px, img->py, rgb);
 		}
 	}
--- src/vec3/init.c
+++ src/vec3/init.c
@@ -6,26 +6,11 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/12 12:56:59 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/12 15:43:57 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/17 23:40:02 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vec3.h"
-
-double	vec3_lensq(t_vec3 *vec)
-{
-	double	lensq;
-
-	lensq = vec->e[0] * vec->e[0];
-	lensq += vec->e[1] * vec->e[1];
-	lensq += vec->e[2] * vec->e[2];
-	return (lensq);
-}
-
-double	vec3_len(t_vec3 *vec)
-{
-	return (sqrt(vec3_lensq(vec)));
-}
 
 t_vec3	*vec3_init(double a, double b, double c)
 {
@@ -39,3 +24,28 @@ t_vec3	*vec3_init(double a, double b, double c)
 	vec->e[2] = c;
 	return (vec);
 }
+
+void	vec3_print(t_vec3 *vec)
+{
+	printf("/ %f \\\n", vec->e[0]);
+	printf("| %f |\n", vec->e[1]);
+	printf("\\ %f /\n", vec->e[2]);
+}
+
+/*
+	t_vec3 *v1 = vec3_init(1, 2, 3);
+	t_vec3 *v2 = vec3_init(30, 20, 10);
+	
+	printf("CROSS\n");
+	vec3_print(vec3_cross(v1, v2));
+	printf("ADD\n");
+	vec3_print(vec3_add(v1, v2));
+	printf("SUBTRACT\n");
+	vec3_print(vec3_subtract(v1, v2));
+	printf("DIVIDE\n");
+	vec3_print(vec3_divide(v1, 3));
+	printf("MULTIPLY\n");
+	vec3_print(vec3_multiply(v1, 3));
+	printf("DOT\n");
+	printf("%f\n", vec3_dot(v1, v2));
+*/

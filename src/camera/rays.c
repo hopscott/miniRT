2cb7c2e6885ec9a22470e2e907674b1aef3139b3
hopscott <scott.willis2706@gmail.com>
2022-08-19 19:01:44 +0200

    #6

--- src/camera/rays.c
+++ src/camera/rays.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/16 20:17:24 by swillis           #+#    #+#             */
-/*   Updated: 2022/08/18 18:57:46 by swillis          ###   ########.fr       */
+/*   Updated: 2022/08/19 19:01:21 by swillis          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,20 +36,38 @@
 //     return hitColor; 
 // } 
 
-int	intersects_sphere(t_vec3 *origin, t_vec3 *direction, t_sphere *sphere) {
+int	plane_intersection(t_vec3 *origin, t_vec3 *direction, t_plane *plane)
+{
+	double	t;
+	t_vec3	*p0;
+	t_vec3	*n;
+	t_vec3	*vec;
+
+	p0 = vec3_init(plane->x, plane->y, plane->z);
+	n = vec3_init(plane->vec_x, plane->vec_y, plane->vec_z);
+	vec = vec3_subtract(p0, origin);
+	t = vec3_dot(vec, n) / vec3_dot(direction, n);
+	free(p0);
+	free(n);
+	free(vec);
+	return (t);
+}
+
+int	sphere_intersection(t_vec3 *origin, t_vec3 *direction, t_sphere *sphere)
+{
 	(void)origin;
 	(void)direction;
 	(void)sphere;
 	return (1);
 }
 
-t_obj_lst	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_obj_lst **objects) {
+t_obj_lst	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_obj_lst **objects)
+{
 	double		t;
 	double		tmin;
 	t_obj_lst	*nearest;
 	t_obj_lst	*elem;
 	t_object	*obj;
-	t_vec3		*vec;
 
 	(void)direction;
 	tmin = INFINITY;
@@ -58,19 +76,28 @@ t_obj_lst	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_obj_lst **obj
 	while (elem)
 	{
 		obj = (t_object *)(elem->content);
-		if ((elem->type == SPHERE) && (intersects_sphere(origin, direction, &obj->sp)))
+		// if ((elem->type == SPHERE) && (sphere_intersection(origin, direction, &obj->sp)))
+		// {
+		// 	vec = vec3_init(obj->sp.x, obj->sp.y, obj->sp.z);
+		// 	t = vec3_distance_points(origin, vec);
+		// 	free(vec);
+		// 	if (t < tmin)
+		// 	{
+		// 		nearest = elem;
+		// 		tmin = t;
+		// 	}
+		// }
+		if (elem->type == SPHERE)
+			;
+		else if (elem->type == PLANE)
 		{
-			vec = vec3_init(obj->sp.x, obj->sp.y, obj->sp.z);
-			t = vec3_distance_points(origin, vec);
-			free(vec);
-			if (t < tmin)
+			t = plane_intersection(origin, direction, &obj->pl);
+			if ((t >= 0) && (t < tmin))
 			{
 				nearest = elem;
 				tmin = t;
 			}
 		}
-		else if (elem->type == PLANE)
-			;
 		else if (elem->type == CYLINDER)
 			;
 		elem = elem->next;
@@ -78,7 +105,8 @@ t_obj_lst	*nearest_hit_object(t_vec3 *origin, t_vec3 *direction, t_obj_lst **obj
 	return (nearest);
 }
 
-t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_space *space) {
+t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_space *space)
+{
 	t_obj_lst		*olst;
 	t_object		*obj;
 	t_vec3			*rgb;
@@ -91,11 +119,14 @@ t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_space *space) {
 		obj = (t_object *)(olst->content);
 		if (olst->type == SPHERE)
 		{
-			printf("nearest obj SPHERE -> xyz(%.1f, %.1f, %.1f)\n", obj->sp.x, obj->sp.y, obj->sp.z);
+			// printf("nearest obj = SPHERE => xyz(%.1f, %.1f, %.1f)\n", obj->sp.x, obj->sp.y, obj->sp.z);
 			rgb = vec3_init(obj->sp.r, obj->sp.g, obj->sp.b);
 		}
 		else if (olst->type == PLANE)
-			;
+		{
+			// printf("nearest obj = PLANE => xyz(%.1f, %.1f, %.1f)\n", obj->pl.x, obj->pl.y, obj->pl.z);
+			rgb = vec3_init(obj->pl.r, obj->pl.g, obj->pl.b);	
+		}
 		else if (olst->type == CYLINDER)
 			;
 	}
@@ -106,7 +137,8 @@ t_vec3	*cast_ray(t_vec3 *origin, t_vec3 *direction, t_space *space) {
 	return (rgb);
 }
 
-unsigned int	rgb_colour(t_vec3 *rgb) {
+unsigned int	rgb_colour(t_vec3 *rgb) 
+{
 	unsigned int	r;
 	unsigned int	g;
 	unsigned int	b;
@@ -158,7 +190,35 @@ unsigned int	rgb_colour(t_vec3 *rgb) {
 // 	}
 // }
 
-// void	space_render_threading()
+double  deg2rad(double degree)
+{
+        return (degree * (M_PI / 180));
+}
+
+void	print_progress(int i, int total)
+{
+	char	*str1;
+	char	*str2;
+
+	if (i < 11)
+	{
+		str1 = ft_strdup("##########");
+		str2 = ft_strdup("          ");
+		printf("\e[?25l\r[%s%s]", &str1[10], &str2[0]);
+		fflush(stdout);
+		free(str1);
+		free(str2);
+	}
+	else if (i % (total / 10) == 0)
+	{
+		str1 = ft_strdup("##########");
+		str2 = ft_strdup("          ");
+		printf("\e[?25l\r[%s%s]", &str1[10 - i % (total / 10)], &str2[i % (total / 10) - 1]);
+		fflush(stdout);
+		free(str1);
+		free(str2);
+	}
+}
 
 void	space_render(t_data *data, int width, int height, t_space *space)
 {
@@ -171,8 +231,12 @@ void	space_render(t_data *data, int width, int height, t_space *space)
 	t_vec3	*direction;
 	t_vec3	*vec;
 	t_vec3	*rgb;
+	double	scale;
+	double	aspect_ratio;
 
 	printf("===== Running =====\n");
+	scale = tan(deg2rad(space->camera->fov / 2));
+	aspect_ratio = (double)width / (double)height;
 	mat = camera_lookat(space->camera);
 	vec = vec3_init(0, 0, 0);
 	origin = vec3_matrix_multiply(mat, vec, 0);
@@ -183,20 +247,21 @@ void	space_render(t_data *data, int width, int height, t_space *space)
 		px = -1;
 		while (++px < width)
 		{
-			x = ((px + 0.5) / width);
-			y = ((py + 0.5) / height);
+			x = (2 * (px + 0.5) / width - 1) * scale * aspect_ratio;
+			y = (1 - 2 * (py + 0.5) / height) * scale;
 			vec = vec3_init(x, y, -1);
 			direction = vec3_matrix_multiply(mat, vec, 1);
 			free(vec);
 			rgb = cast_ray(origin, direction, space);
 			free(direction);
 			my_mlx_pixel_put(data, px, py, rgb_colour(rgb));
-			printf("[%d][%d] => %X\n", px, py, rgb_colour(rgb));
-			// vec3_print(rgb);
+			printf("\r[%d][%d] => %X", px, py, rgb_colour(rgb));
+			// printf("> %i / %i\n", py, height);
+			// print_progress(py, height);
 			free(rgb);
 		}
 	}
 	free(mat);
 	free(origin);
-	printf("===== Finished =====\n");
+	printf("\n===== Finished =====\n");
 }

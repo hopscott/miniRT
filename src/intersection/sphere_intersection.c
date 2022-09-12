a92f7b7a5e8930c5981e5a1fdcf2cc4827777452
Ouafae MOUDNI <omoudni@e1r9p4.clusters.42paris.fr>
2022-09-12 13:43:46 +0200

    ready to start norming

--- src/intersection/sphere_intersection.c
+++ src/intersection/sphere_intersection.c
@@ -6,7 +6,7 @@
 /*   By: swillis <swillis@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/17 23:03:34 by omoudni           #+#    #+#             */
-/*   Updated: 2022/09/09 18:58:56 by swillis          ###   ########.fr       */
+/*   Updated: 2022/09/11 20:50:40 by omoudni          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,12 +91,20 @@ void	sphere_intersection(t_ray *ray, t_sphere *sp, t_hit *hit)
 		hit->t = get_short_dist(discriminant, ab[0], ab[1]);
 }
 
-t_vec3	*sphere_surface_normal(t_sphere *sphere, t_vec3 *phit)
+t_vec3	*sphere_surface_normal(t_ray *ray, t_sphere *sphere, t_vec3 *phit)
 {
 	t_vec3	*tmp;
 	t_vec3	*normal;
 
-	tmp = vec3_subtract(phit, sphere->xyz);
+	printf("I entered here\n");
+
+	if (vec3_distance_points(ray->origin, sphere->xyz) < sphere->diameter / 2)
+	{
+		printf("I entered here\n");
+		tmp = vec3_subtract(sphere->xyz, phit);
+	}
+	else
+		tmp = vec3_subtract(phit, sphere->xyz);
 	normal = vec3_unit(tmp, 1);
 	return (normal);
 }
